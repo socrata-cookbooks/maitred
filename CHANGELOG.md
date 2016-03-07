16e0bcc0be637bad1356c7c4b390076b4ca010c8
Jonathan Hartman <j@p4nt5.com>
2016-03-07 15:48:24 -0800

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Socrata Chef Server Cookbook CHANGELOG
+======================================
+
+v0.0.1 (2016-03-07)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+# Encoding: UTF-8
+
+name             'socrata-chef-server'
+maintainer       'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license          'apache2'
+description      'Installs/Configures socrata-chef-server'
+long_description 'Installs/Configures socrata-chef-server'
+version          '0.0.1'
